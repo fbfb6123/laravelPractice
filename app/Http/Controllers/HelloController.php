2c6e9a7da9505b4a9845ff6f1967fbf7f8ee0f1f
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-24 23:36:55 +0900

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index() {
+        $data = [
+            'msg'=>'this is sample message.',
+        ];
+    }
 }
