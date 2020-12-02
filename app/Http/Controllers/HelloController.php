981809e7e9075d8f77dfb3d23d494cb1d54f0859
fbfb6123 <fbfb6123@gmail.com>
2020-12-02 17:06:27 +0900

    HelloControllerにMyServiceを適用　引数に追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,28 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\MyClasses\MyService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 
-
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public function index(MyService $myService)
     {
-
-            $name = $request->query('name');
-            $mail = $request->query('mail');
-            $tel = $request->query('tel');
-            $msg = $name . ', ' . $mail .', ' .$tel;
-            $keys = ['名前','メール','電話'];
-            $values = [$name, $mail, $tel];
             $data =[
-                'msg'=>$msg,
-                'keys' => $keys,
-                'values' => $values,
+                'msg' =>$myService->say(),
+                'data' =>$myService->data()
             ];
-            $request->flash();
+
         return view('hello.index',$data);
     }
 
