01884286620a66ffac455d0ae22b3f9bda00d96e
fbfb6123 <fbfb6123@gmail.com>
2020-12-09 17:15:01 +0900

    say data setData malldataメソッドの追記

--- app/MyClasses/PowerMyService.php
+++ app/MyClasses/PowerMyService.php
@@ -21,5 +21,24 @@ class PowerMyService implements MyServiceInterface
                 . "番の" .$this->data[$id] . "ですね";
         }
     }
-    
+
+    public function say()
+    {
+        return $this->msg;
+    }
+
+    public function data(int $id)
+    {
+        return $this->data[$id];
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    public function alldata()
+    {
+        return $this->data;
+    }
 }
