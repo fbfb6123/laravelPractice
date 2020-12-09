07003d31100a1da522a1f8638efba9d5543d90ce
fbfb6123 <fbfb6123@gmail.com>
2020-12-09 14:55:25 +0900

    インターフェイス生成

---
+++ app/MyClasses/MyServiceinterface.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\MyClasses;
+
+interface MyServiceinterface
+{
+    public function setId(int $id);
+    public function say();
+    public function allData();
+    public function data(int $id);
+}
