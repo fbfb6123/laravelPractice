2af0bd3f5bc63173633fb9327fb8c5af0410c249
fbfb6123 <fbfb6123@gmail.com>
2020-12-10 17:27:03 +0900

    クエリビルダの動作確認用のview生成

---
+++ resources/views/hello2/index.blade.php
@@ -0,0 +1,22 @@
+{{--DBとクエリビルダの動作確認用--}}
+<!doctype html>
+<html lang="ja">
+<head>
+    <title>Index</title>
+</head>
+<body>
+<h1>Hello/Index</h1>
+<p>{{$msg}}</p>
+<ol>
+    @foreach($data as $item)
+        <li>{{$item->name}} [{{$item->mail}},{{$item->age}}</li>
+    @endforeach
+</ol>
+</body>
+</html>
+
+
+<style>
+    th  { background-color: red; padding: 10px; }
+    td  { background-color: #eee; padding: 10px; }
+</style>
