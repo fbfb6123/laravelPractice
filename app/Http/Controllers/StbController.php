d92e365c194597837e9ac8cee1451a7faa45efc4
fbfb6123 <fbfb6123@gmail.com>
2020-12-14 10:53:38 +0900

    Log::info($result) Log::debug($data) 配列確認

--- app/Http/Controllers/StbController.php
+++ app/Http/Controllers/StbController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class StbController extends Controller
 {
@@ -13,6 +14,19 @@ class StbController extends Controller
         {
             $msg = 'get name like "' .$id . '".';
             $result = [DB::table('people')->find($id)];
+            Log::info($result);
+            //ログ($result)の中身 ２次元配列
+            /*local.INFO: array (
+            0 =>
+                (object) array(
+                    'id' => 3,
+                    'name' => '永田',
+                    'email' => 'zoo@zoo',
+                    'age' => 28,
+                    'created_at' => '2020-12-14 09:51:00',
+                    'updated_at' => '2020-12-14 09:51:01',
+                ),
+        )  */
         }
         else
         {
@@ -24,6 +38,22 @@ class StbController extends Controller
             'msg' =>$msg,
             'data' => $result,
         ];
+        Log::debug($data);
+        //ログ($data)の中身　　三次元配列
+        /*local.DEBUG: array (
+        'msg' => 'get name like "3".',
+        'data' =>
+            array (
+                0 =>
+                    (object) array(
+                        'id' => 3,
+                        'name' => '永田',
+                        'email' => 'zoo@zoo',
+                        'age' => 28,
+                        'created_at' => '2020-12-14 09:51:00',
+                        'updated_at' => '2020-12-14 09:51:01',
+                    ),
+            ),*/
 
         return view('hello2.index', $data);
     }
