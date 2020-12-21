29a38e63dfc58d7449ffdc8a6e03e43fd43ba812
fbfb6123 <fbfb6123@gmail.com>
2020-12-21 11:23:04 +0900

    MyPaginator生成

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Person;
 
 class Person extends Model
 {
-    use HasFactory;
+$result = Person::paginate(3);
 }
