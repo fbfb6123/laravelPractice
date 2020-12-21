3bb9396220aa385e5308b8d099ac61cc0a1d739a
fbfb6123 <fbfb6123@gmail.com>
2020-12-21 17:25:42 +0900

    diffによる差分取得　50歳未満かつ20歳未満のレコード取得

--- app/Http/Controllers/Stb2Controller.php
+++ app/Http/Controllers/Stb2Controller.php
@@ -12,14 +12,21 @@ class Stb2Controller extends Controller
     {
         $msg = 'peopleテーブル';
 
-        $result = Person::get()->reject(function($person)
+        $result = Person::get()->filter(function($person)
         {
-            return $person->age <25;
+            return $person->age < 50;
         });
+
+        $result2 = Person::get()->filter(function ($person)
+        {
+            return $person->age < 20;
+        });
+
+        $result3 = $result->diff($result2);
 
         $data = [
             'msg' =>$msg,
-            'data' =>$result,
+            'data' =>$result2,
         ];
 
         return view('hello3.index', $data);
