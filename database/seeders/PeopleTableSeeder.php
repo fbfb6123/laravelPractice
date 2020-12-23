36560090f789c14d00da2b3d2127c56a133f19d9
fbfb6123 <fbfb6123@gmail.com>
2020-12-23 14:07:44 +0900

    テスト用のseeder作成

---
+++ database/seeders/PeopleTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert(
+            [
+                'name' => Str::random(10),
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        DB::table('people')->insert(
+            [
+                'name' => 'ikeda',
+                'email' => 'ikeda@ikeda',
+                'age' => '24',
+            ]
+        );
+    }
+}
